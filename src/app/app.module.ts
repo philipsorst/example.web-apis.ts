0f1b0500e5bdbb448c9a29cae98158e4279ac46c
Philip Washington Sorst <philip@sorst.net>
2020-11-12 22:14:39 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {DdrExtensionsModule} from '@dontdrinkandroot/ngx-extensions';
+import {DdrMaterialExtensionsModule} from '@dontdrinkandroot/ngx-material-extensions';
+import {IndexComponent} from '../index/index.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        IndexComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+        MatToolbarModule,
+        MatListModule,
+        MatSidenavModule,
+        DdrExtensionsModule,
+        DdrMaterialExtensionsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule
+{
+}
