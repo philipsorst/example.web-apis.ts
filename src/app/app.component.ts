92153295acaca72cd16218786e6b92e8bf6e3018
Philip Washington Sorst <philip@sorst.net>
2020-11-14 21:15:39 +0100

    Adding service worker update detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
+    constructor(private swUpdate: SwUpdate, private snackBar: MatSnackBar)
+    {
+    }
+
+    /**
+     * @override
+     */
+    public ngOnInit()
+    {
+        this.swUpdate.available.subscribe(evt => {
+            const snack = this.snackBar.open('Update Available', 'Reload', {duration: 5000});
+            snack
+                .onAction()
+                .subscribe(() => {
+                    window.location.reload();
+                });
+        });
+    }
 }
